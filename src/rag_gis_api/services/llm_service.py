4483f1940f8e14aefe75fe695980af6c498788a6
Shinatip Pattanapitoon <shinatip.ptn@gmail.com>
2026-08-14 17:03:06 +0700

    feat: add lru cache to llm service

--- src/rag_gis_api/services/llm_service.py
+++ src/rag_gis_api/services/llm_service.py
@@ -1,8 +1,11 @@
+from functools import lru_cache
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from rag_gis_api import GOOGLE_API_KEY
 
 
+@lru_cache(maxsize=1)
 def get_llm() -> ChatGoogleGenerativeAI:
     return ChatGoogleGenerativeAI(
         model="gemini-2.5-flash",
